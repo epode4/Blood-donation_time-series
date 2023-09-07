6941feda5cf2d86c126445d57c35bc2621e2ede4
epode4 <epode4@gmail.com>
2023-09-07 11:34:09 +0900

    데이터 불러온 뒤 확인 및 시계열 데이터로 변환

--- blood_dona_prediction.R
+++ blood_dona_prediction.R
@@ -22,3 +22,13 @@ test <- function(train_a,fita,a){
   ggtsdisplay(res_a,plot.type = "histogram")
   Box.test(res_a,fitdf=df_a,lag=lag_a,type="Ljung-Box")
 }
+
+
+blood <- read_xlsx("C:/Users/Seain/Desktop/공부하긔/4-1/시계열분석/report/헌혈현황(월별).xlsx")
+blood |> head()
+blood |> tail()
+
+blood_ts <- ts(as.numeric(blood$합계),start=c(2005,1),end=c(2021,12),freq=12)
+blood_ts |> autoplot()
+blood_ts |> decompose() |> autoplot()
+
